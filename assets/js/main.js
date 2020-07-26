921be714d3220ec1295e0e3f51b08e562e556e32
Benjamin Haeberli <hello@benjaminhaeberli.ch>
2020-07-26 12:23:52 +0200

    feat: experience + projects section (static)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-var rellax = new Rellax('.rellax', {});
+var element = document.getElementById('experience');
+
+document.body.onscroll = function () {
+    var scrollY = window.scrollY;
+    var section__about = document.getElementById('about');
+    if (scrollY > 150) {
+        section__about.classList.add('unfocused');
+    } else {
+        document.getElementById('about').classList.remove('unfocused');
+    }
+
+    var scaleAmt = 1 / (1 + scrollY / 2000);
+    section__about.setAttribute('style', 'transform : scale(' + scaleAmt + ')');
+};
